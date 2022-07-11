ab130dec5d45d98d5bd491bd84c74a3e2ce123ce
Ricardo <85304541+RichardDB7@users.noreply.github.com>
2022-07-11 15:36:40 -0300

    Update PruebaConexión.java

--- PaqueteDatabase/PruebaConexión.java
+++ PaqueteDatabase/PruebaConexión.java
@@ -3,8 +3,11 @@ package Database;
 public class PruebaConexion {
 	public static void main(String[] args) {
 		
+		//Creo un objeto de tipo conexión y su instancia
 		Conexion con = Conexion.getInstancia();
+		//Llamada al método conectar
 		con.conectar();
+		//Condicional para comprobar el estado de la conexión
 		if(con.cadena!=null) {
 			System.out.println("Conectado");
 		}
